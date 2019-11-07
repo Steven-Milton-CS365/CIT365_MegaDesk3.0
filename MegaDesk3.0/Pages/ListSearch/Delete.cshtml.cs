b3aa23b88a737d3280bce9311e5a3dada0327ad3
Morris-Trevon <tlove21@gmail.com>
2019-11-07 12:49:06 -0500

    Created CustomerSearch, with Sort of customers, Search by Customer Name, and Pagination after 10 entries. Add a Pagination List that can be used on any screen that needs it. Edited the layout to show the menu items properly.

---
+++ MegaDesk3.0/Pages/ListSearch/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MegaDesk3._0.Data;
+using MegaDesk_Asp.Net.Model;
+
+namespace MegaDesk3._0.Pages.ListSearch
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly MegaDesk3._0.Data.MegaDesk3_0Context _context;
+
+        public DeleteModel(MegaDesk3._0.Data.MegaDesk3_0Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public DeskQuote DeskQuote { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            DeskQuote = await _context.DeskQuote.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (DeskQuote == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            DeskQuote = await _context.DeskQuote.FindAsync(id);
+
+            if (DeskQuote != null)
+            {
+                _context.DeskQuote.Remove(DeskQuote);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
